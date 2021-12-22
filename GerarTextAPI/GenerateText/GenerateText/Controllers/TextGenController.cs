1626e23e7f96cf55b8e4e17e7e2141a641f3f0d2
carlosmartins77 <carlosmatrins@gmail.com>
2021-12-22 17:36:32 +0000

    Multiple thingsXD

---
+++ GerarTextAPI/GenerateText/GenerateText/Controllers/TextGenController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenerateText.Controllers
+{
+    public class TextGenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Criar uma variavel global random
+        /// </summary>
+        private static Random random = new Random();
+
+        /// <summary>
+        /// Dada uma lista de caracteres, retorna uma string aleatória
+        /// </summary>
+        /// <returns> Uma string aleatória</returns>
+        [HttpGet("RandomText")]
+        public string RandomText()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return "SKU " + new string(Enumerable.Repeat(chars, 12).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
